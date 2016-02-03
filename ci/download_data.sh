7854f955615f0d997af4dc4a486f260af994b921
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-02-03 14:34:13 +0100

    Fix download path issue in ci.

--- ci/download_data.sh
+++ ci/download_data.sh
@@ -3,6 +3,6 @@
 pushd aupyom/example_data
     for f in *.wav; do
         echo "Downloading $f..."
-        wget https://media.githubusercontent.com/media/pierre-rouanet/aupyom/$TRAVIS_BRANCH/aupyom/example_data/$f -O $f
+        wget "https://media.githubusercontent.com/media/pierre-rouanet/aupyom/$TRAVIS_BRANCH/aupyom/example_data/$f" -O "$f"
     done
 popd
